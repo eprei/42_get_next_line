3a7f05e82f0c4f478cc03baa769b4a26507f8c2c
Emiliano Presa custodio <epresa-c@c2r9s6.42lausanne.ch>
2021-12-20 17:00:19 +0100

    segundo

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,30 @@
 /*   By: epresa-c <epresa-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 14:40:14 by epresa-c          #+#    #+#             */
-/*   Updated: 2021/12/20 14:41:51 by epresa-c         ###   ########.fr       */
+/*   Updated: 2021/12/20 16:41:45 by epresa-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
-{
+#define BUF_SIZE 4096
 
-    
+char	*get_next_line(int fd)
+{
+	int		ret;
+	char	buf_dest[BUF_SIZE + 1];
+	char	*ptr;
+	int		i;
+
+	i = 0;
+	ret = read(fd, buf_dest, BUF_SIZE);
+	buf_dest[ret] = "\0";
+	ptr = (char *)malloc(ret + 1);
+	if (! ptr)
+		return (-1);
+	while (buf_dest[i] != '\0')
+	{
+		ptr[i] == *buf_dest;
+		i++;
+	}
+	free(ptr);
+	return (ptr);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,18 @@
 /*   By: epresa-c <epresa-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 14:40:39 by epresa-c          #+#    #+#             */
-/*   Updated: 2021/12/20 14:42:35 by epresa-c         ###   ########.fr       */
+/*   Updated: 2021/12/20 16:41:46 by epresa-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE.H
-# define GET_NEXT_LINE.H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-char *get_next_line(int fd);
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
 
 #endif
