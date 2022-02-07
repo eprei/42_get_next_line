89e419eb0ad8e3e34cff6f4d632fed2ac1872ec3
eprei <93380002+eprei@users.noreply.github.com>
2022-02-07 19:04:45 +0100

    sommeComments

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: Emiliano <Emiliano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 14:40:14 by epresa-c          #+#    #+#             */
-/*   Updated: 2022/02/07 11:12:28 by Emiliano         ###   ########.fr       */
+/*   Updated: 2022/02/07 19:04:14 by Emiliano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,9 @@ char	*get_next_line(int fd)
 	char			buf[BUFFER_SIZE + 1];
 	static char		*line = NULL;
 	char			*tmp;
-	char			*tmp_buf;
+	char			*reste_du_texte;
 	size_t			line_size;
 	static char		*line_returned = NULL;
-//	size_t			i;
 
 	if (fd < 0 || fd > 999 /*why tripouille find that fd 1000 return has to be NULL?*/ || BUFFER_SIZE < 1)
 		return (0);
@@ -56,8 +55,7 @@ char	*get_next_line(int fd)
 	{
 		ret = read(fd, buf, BUFFER_SIZE);
 		buf[ret] = '\0';
-		if (ret < 0) // funciona con EMPTY FILES pero cuando el 
-					 // archivo esta lleno y llega a EOF y READ no lee mas: no puede ser return NULL
+		if (ret < 0) 
 			return (NULL);
 		if (line == NULL)
 			line = ft_strdup(buf);
@@ -71,25 +69,24 @@ char	*get_next_line(int fd)
 		if (ft_strchr(line, '\n'))
 			break;
 	}
-/*	while (line[i] != '\n' && line[i] != '\0')
-		i++;
-	*/
 	if (line[0] == 0)
 	{ 
-	//	free(line_returned);
-		free(line);
+		if (line != NULL)
+		{
+			free(line);
+			line = NULL;
+		}
 		return (NULL);
 	}
-//	if (ret < BUFFER_SIZE && !ft_strchr(line, '\n')) //cuando es el ultimo fragmento antes de EOF
-//		return (line);
 	else
 	{
-		line_size = line_len(line); //medir linea
-		line_returned = ft_substr(line, 0, line_size + 1); //extrae nueva linea y la guarda en line_returned
-		tmp_buf = ft_strdup(&(line)[line_size + 1]); //extrae el resto y lo gaurda en tmp_buf
-		free(line); //free linea
-		line = tmp_buf; //copia el resto en linea
-		tmp_buf = NULL; //regresa tmp_buf a cero
+		line_size = line_len(line); // mesurer la ligne
+		line_returned = ft_substr(line, 0, line_size + 1); // extrait la nouvelle ligne et la stocke dans line_returned
+		reste_du_texte = ft_strdup(&(line)[line_size + 1]); // extraire le reste de la ligne et l'enregistrer dans reste_du_texte
+		free(line);
+//		line = NULL;
+		line = reste_du_texte;
+//		reste_du_texte = NULL;
 		return (line_returned);
 	}
 }
