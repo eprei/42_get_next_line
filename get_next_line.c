5362d56968a42b165ef4225d8e6b35e89810d88a
Emiliano Presa custodio <epresa-c@c2r9s6.42lausanne.ch>
2022-02-16 12:48:39 +0100

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Emiliano <Emiliano@student.42.fr>          +#+  +:+       +#+        */
+/*   By: epresa-c <epresa-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 14:40:14 by epresa-c          #+#    #+#             */
-/*   Updated: 2022/02/15 18:07:30 by Emiliano         ###   ########.fr       */
+/*   Updated: 2022/02/16 12:45:57 by epresa-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,59 +38,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substr[i] = '\0';
 	return (substr);
 }
-/*
-char	*get_next_line(int fd)
-{
-	static ssize_t	ret = BUFFER_SIZE;
-	char			buf[BUFFER_SIZE + 1];
-	static char		*line = NULL;
-	char			*tmp;
-	char			*reste_du_texte;
-	size_t			line_size;
-	static char		*line_returned = NULL;
 
-	if (fd < 0 || fd > 999 why tripouille find that fd 1000 return has to be NULL? || BUFFER_SIZE < 1)
-		return (0);
-	while (ret > 0)
-	{
-		ret = read(fd, buf, BUFFER_SIZE);
-		buf[ret] = '\0';
-		if (ret < 0) 
-			return (NULL);
-		if (line == NULL)
-			line = ft_strdup(buf);
-		else
-		{
-			tmp = ft_strjoin(line, buf);
-			free(line);
-			line = tmp;
-			tmp = NULL;
-		}
-		if (ft_strchr(line, '\n'))
-			break;
-	}
-	if (line[0] == 0)
-	{ 
-		if (line != NULL)
-		{
-			free(line);
-			line = NULL;
-		}
-		return (NULL);
-	}
-	else
-	{
-		line_size = line_len(line); // mesurer la ligne
-		line_returned = ft_substr(line, 0, line_size + 1); // extrait la nouvelle ligne et la stocke dans line_returned
-		reste_du_texte = ft_strdup(&(line)[line_size + 1]); // extraire le reste de la ligne et l'enregistrer dans reste_du_texte
-		free(line);
-		line = NULL;
-		line = reste_du_texte;
-		reste_du_texte = NULL;
-		return (line_returned);
-	}
-}
-*/
 char	*get_next_line(int fd)
 {
 	static char		*storage;
@@ -99,16 +47,16 @@ char	*get_next_line(int fd)
 	int				line_size;
 	char			*tmp;
 	ssize_t			ret;
-	
+
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (0);
 	ret = read(fd, buf, BUFFER_SIZE);
-	if (ret < 0) 
+	if (ret < 0)
 		return (NULL);
 	if (ret == 0 && storage == NULL)
 	{
 		free(next_line);
-		next_line = NULL;	
+		next_line = NULL;
 		return (next_line);
 	}
 	buf[ret] = '\0';
@@ -116,35 +64,25 @@ char	*get_next_line(int fd)
 	{
 		if (storage == NULL)
 			storage = ft_strdup("");
-		else 
+		else
 		{
 			tmp = ft_strjoin(storage, buf);
 			free(storage);
 			storage = tmp;
 			tmp = NULL;
 			if (ft_strchr(buf, '\n'))
-				break;
+				break ;
 			ret = read(fd, buf, BUFFER_SIZE);
 			buf[ret] = '\0';
 		}
 	}
 	if (storage != NULL) // si hay algo guardado en storage, copiamos nueva linea a next_line
 	{
-		if (next_line != NULL) // si existe next_line, la liberamos
-		{
-			free(next_line); // free next_line en caso de que existiera previamente
-			next_line = NULL; // llevarla a null para evitar eventuales doble free
-		}
 		line_size = line_len(storage); // medir la linea
 		if (ft_strchr(storage, '\n')) // si hay '\n' copiar hasta el '\n' (line_size + 1)
 			next_line = ft_substr(storage, 0, line_size + 1);
-		if (!ft_strchr(storage, '\n')) // si no hay retorno a la linea, entonces es la ultima linea y no se copia un caracter mas. solo hasta (line_size);
-		{
+		if (!ft_strchr(storage, '\n') && line_size != 0) // si no hay retorno a la linea, entonces es la ultima linea y no se copia un caracter mas. solo hasta (line_size);
 			next_line = ft_substr(storage, 0, line_size);
-			free(storage);
-			storage = NULL;
-			return (next_line);
-		}
 	}	
 	if (ft_strchr(storage, '\n') && storage != NULL) // si hay nueva linea entonces decalar storage
 	{
@@ -152,40 +90,21 @@ char	*get_next_line(int fd)
 		free(storage);
 		storage = tmp;
 		tmp = NULL;
-		return(next_line);
+		return (next_line);
 	}
-/*	if (!ft_strchr(storage, '\n') && storage != NULL) // si no hay nueva linea, entonces es la ultima linea, entonces borramos el storage y devolvemos la linea
+	if (!ft_strchr(storage, '\n') && storage != NULL) // si no hay nueva linea, entonces es la ultima linea, entonces borramos el storage y devolvemos la linea
 	{
 		free(storage);
 		storage = NULL;
 		return (next_line);
 	}
-*/	if (next_line != NULL)
+	if (next_line != NULL)
 	{
 		free(next_line);
 		next_line = NULL;
 	}
-	return(NULL);
+	return (NULL);
 }
-	
-/* IDEA TEXTUAL
-	if (fd < 0 || BUFFER_SIZE < 1)
-		return (0);
-	ret = read(fd, buf, BUFFER_SIZE);
-	if (ret > 0)
-		llamar funcion que copie buf en la linea_larga usando read y que se termine cuando strchr(buf, '\n') o cuando no haya mas nada para leer: ret = 0;
-	llamar funcion que copia nueva linea en next_line
-		1 si existe next_line y next_line != NULL: free(next_line) y next_line = NULL.
-		2 si existe linea_larga y linea_larga != NULL: guarde en next_linea la nueva linea obteniendola de linea.
-			1 Si strchr(linea_larga, '\n'), quiere decir que hay varias lineas y entonces copia hasta n + 1 o solo copia hasta n y luego agrega un '\n' al final.
-			2 si !strch(linea_larga, '\n') quiere decir que es la ultima linea y copia hasta n.
-	llamar funcion que decale la linea_larga
-		si existe linea_larga y linea_larga != NULL;
-		1 si strchr(linea_larga, '\n') entonces hay nueva linea y hay que decalar linea_larga usando substr() [ver linea 84 de codigo actual]
-		2 si !strch(linea_larga, '\n') entonces no hay nueva linea. free(linea_larga) y linea_larga = NULL;
-	return(next_line);
-}
-*/
 /*
 int main()
 {
@@ -194,19 +113,19 @@ int main()
 
 	fd = open("TextToRead", O_RDONLY);
 	linea = get_next_line(fd);
-	printf("%s", linea);
+	printf("1: %s", linea);
 	linea = get_next_line(fd);
-	printf("%s", linea);
+	printf("2: %s", linea);
 	linea = get_next_line(fd);
-	printf("%s", linea);
+	printf("3: %s", linea);
 	linea = get_next_line(fd);
-	printf("%s", linea);
+	printf("4: %s", linea);
 	linea = get_next_line(fd);
-	printf("%s", linea);
+	printf("5: %s", linea);
 	linea = get_next_line(fd);
-	printf("%s", linea);
+	printf("6: %s", linea);
 	linea = get_next_line(fd);
-	printf("%s", linea);
+	printf("7: %s", linea);
 	linea = get_next_line(fd);
 	printf("%s", linea);
 	linea = get_next_line(fd);
@@ -216,4 +135,5 @@ int main()
 	linea = get_next_line(fd);
 	printf("%s", linea);
 	return (0);
-}*/
+}
+*/
